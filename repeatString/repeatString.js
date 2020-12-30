b624bf784d946ef743758466a7b8d46048830219
Fred Sites <fsites521@gmail.com>
2020-12-30 15:25:16 -0800

    first few TDD challenges done

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
 
+	let output = ''
+	for (let i = 0; i < times; i++) {
+		output += word
+	}
+
+	return output
 }
 
 module.exports = repeatString
