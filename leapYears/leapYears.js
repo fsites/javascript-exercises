b067018246120ebda586b00b76d125335f48fa18
Fred Sites <fsites521@gmail.com>
2020-12-30 16:16:21 -0800

    working through leap year test

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,4 +1,21 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+//if divisble by 100 and not divisble by 400 = not leap year
+//if divisble by 4 and 400 = leap year
+//if divisble by 4 = leap year
+//else not a leap year
+
+	if (year % 100 == 0 && year % 400 !==0) {
+		return false;
+	} else if (year % 4 == 0 && year % 400 == 0) {
+		return true;
+	} else if (year % 4 == 0) {
+		return true
+	} else {
+		return false;
+	}
+
+
 
 }
 
