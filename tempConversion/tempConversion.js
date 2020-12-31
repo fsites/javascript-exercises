842d393561fa9c67d180ae78be7e229f26a38ead
Fred Sites <fsites521@gmail.com>
2020-12-30 16:28:20 -0800

    finished all TDD exercises

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fah) {
+	let cel;
+	cel = (fah - 32) * (5/9);
+	return Math.round(cel * 10) / 10;
 }
 
-const ctof = function() {
+const ctof = function(cel) {
+	let fah;
+	fah  = cel * (9/5) + 32;
+	return Math.round(fah * 10) / 10;
 
 }
 
